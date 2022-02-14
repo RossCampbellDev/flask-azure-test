9cd73b895e91eb1400cca3cab9bd4a1d97ff4ad7
rcampbell-sec <rcampbell_sec@protonmail.com>
2022-02-14 15:20:32 +0000

    testing download from blob

--- azureblobstorage.py
+++ azureblobstorage.py
@@ -1,7 +1,7 @@
-import os,uuid
+import os, uuid
 from azure.storage.blob import BlobServiceClient, BlobClient, ContainerClient, __version__
 
-print("azure blob storage v: " + __version__) # test its imported
+print("azure blob storage v: " + __version__)  # test it's imported
 
 key = "DDLGr0r+lRnIYJv1YGRpWr0sS/IFg0avQzj6jX+FlqAtT62cESoUCjn99nOkXfmELZ8cH5u6UA1gKAdPXcLFXQ=="
 acct_name = "flaskstorageaccount"
@@ -11,11 +11,21 @@ conn_string = "BlobEndpoint=https://flaskteststorage.blob.core.windows.net/;Queu
 try:
     blob_service_client = BlobServiceClient.from_connection_string(conn_string)  # create a client to interact with the blob service
     container_name = str(uuid.uuid4())  # create a uuid for a container
-    container_client = blob_service_client.create_container(container_name)
+    # container_client = blob_service_client.create_container(container_name)  # creating new container
     local_file_name = "test.txt"
 
-    blob_client = blob_service_client.get_blob_client(container_name, blob=local_file_name)
-    with open(local_file_name, "rb") as data:
-        result = blob_client.upload_blob(data)
+    container_client = blob_service_client.get_container_client('729c45f7-db98-4c12-ab37-ab59ece225a3')
+    # blob_client = blob_service_client.get_blob_client(container_name, blob=local_file_name)
+
+    # with open(local_file_name, "rb") as data:
+    #     result = blob_client.upload_blob(data)
+
+    blob_list = container_client.list_blobs()
+    for blob in blob_list:
+        print('\t' + blob.name)
+
+    download_file_path = os.path.join('/', str.replace('test.txt','.txt','DOWNLOAD.txt'))
+    with open(download_file_path, "wb") as download_file:
+        download_file.write(container_client.download_blob.readall())
 except Exception as e:
     print(e)
